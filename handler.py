a721a0e132ae8238c1d2437fa9d5e7633f5ffa6c
Ahmed Elhaddad <5328478+aihaddad@users.noreply.github.com>
2020-04-17 15:50:52 +0300

    Implement contact form service

--- handler.py
+++ handler.py
@@ -1,33 +1,46 @@
+import os
 import json
+import boto3
 from serverless_sdk import tag_event
 
+SENDER = os.getenv("SENDER")
+RECEIVER = os.getenv("RECEIVER")
 
-def hello(event, context):
-    tag_event('custom-tag', 'hello-world', {'custom': {'tag': 'data '}})
+mailer = boto3.client("ses")
+
+
+def contact(event, context):
+    tag_event("custom-tag", "contact-form", {"custom": {"tag": "data "}})
+
+    print(event)
+    data = json.loads(event.get("body"))
+    full_name, email = data.get("FullName"), data.get("Email")
+    subject, message = data.get("Subject"), data.get("Message")
+
+    mailer.send_email(
+        Source=SENDER,
+        Destination={"ToAddresses": [RECEIVER]},
+        Message={
+            "Subject": {"Data": f"[Site] {subject}", "Charset": "utf-8"},
+            "Body": {
+                "Text": {
+                    "Data": "Message from: {}, email: {}:\n---\n{}".format(
+                        full_name, email, message
+                    ),
+                    "Charset": "utf-8",
+                }
+            },
+        },
+    )
 
     headers = {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": True
     }
 
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "message": "Thank you for your email",
     }
 
-    response = {
-        "statusCode": 200,
-        "headers": headers,
-        "body": json.dumps(body)
-    }
+    response = {"statusCode": 200, "headers": headers, "body": json.dumps(body)}
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
